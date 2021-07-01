aad046cfcfe5820094c33f1e306c0d9e97eb6fd5
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2021-07-02 01:43:02 +0200

    removed comment in discordrpc file

--- Assets/nanoSDK_AutomaticUpdater/DiscordRpc/DiscordRpc.cs
+++ Assets/nanoSDK_AutomaticUpdater/DiscordRpc/DiscordRpc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
 using AOT;
-//indev section
+
 public class DiscordRpc
 {
     [MonoPInvokeCallback(typeof(OnReadyInfo))]
