0f89ac96f5e14ba1984b77a531be531e337a2dc3
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2021-07-19 01:03:47 +0200

    == V1.4.4 == VRCSDK3-AVATAR-2021.04.21.11.58 (Used as basis)     ┠ Fixed         ┖ NullReferenceException in GradientScroll Script     ┠ Added         ┖ V1.4.3.Beta Features (except unity 2019Version) Read More in Commit.

--- Assets/VRCSDK/nanoSDK/Scripts/nanoSDK_GradientScroll.cs
+++ Assets/VRCSDK/nanoSDK/Scripts/nanoSDK_GradientScroll.cs
@@ -2,32 +2,41 @@
 using UnityEngine;
 using UnityEngine.UI;
 using nanoSDK;
+using System.Collections.Generic;
 
-
-namespace nanoSDK {
+namespace nanoSDK
+{
     public class nanoSDK_GradientScroll : MonoBehaviour
     {
         public Toggle myToggle;
-        public Text excludeText;
+        public Text doBlackText;
         public Gradient myGradient;
         public float strobeDuration = 5f;
-        public Text go;
+        public Text rainbowText;
 
 
         public void FixedUpdate()
         {
-            //if (UITextRainbow)
+            try
             {
+                if (doBlackText || rainbowText || myToggle == null)
+                    //Ignore
+
                 if (myToggle.isOn)
                 {
                     float t = Mathf.PingPong(Time.time / strobeDuration, 1f);
-                    go.color = myGradient.Evaluate(t);
+                    rainbowText.color = myGradient.Evaluate(t);
                 }
                 else
                 {
-                    go.color = Color.white;
-                    excludeText.color = Color.black;
+                    rainbowText.color = Color.white;
+                    doBlackText.color = Color.black;
+
                 }
+            }
+            catch (System.NullReferenceException)
+            {
+                //ignore
             }
         }
     }
