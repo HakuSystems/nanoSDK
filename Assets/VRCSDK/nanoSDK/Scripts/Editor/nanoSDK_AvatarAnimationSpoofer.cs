453baec18583ba496b88b4fc8abfbe259449a7aa
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2022-05-26 21:22:55 +0200

    Test push Hashing tests

--- Assets/VRCSDK/nanoSDK/Scripts/Editor/nanoSDK_AvatarAnimationSpoofer.cs
+++ Assets/VRCSDK/nanoSDK/Scripts/Editor/nanoSDK_AvatarAnimationSpoofer.cs
@@ -1,7 +1,9 @@
 ﻿using nanoSDK;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
@@ -30,7 +32,62 @@ namespace nanoSDK
             {
                 AvatarSelectSave();
             }
+
+
+            
+            if (GUILayout.Button("HASH"))
+            {
+                CreateMd5ForFolder($"Assets{Path.DirectorySeparatorChar}VRCSDK{Path.DirectorySeparatorChar}nanoSDK");
+                
+            }
         }
+
+        public static string CreateMd5ForFolder(string path)
+        {
+            // assuming you want to include nested folders
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                                 .OrderBy(p => p).ToList();
+            //foreach if meta file is found
+            for (int i = 0; i < files.Count; i++)
+            {
+                if (!files[i].EndsWith(".cs"))
+                {
+                    files.RemoveAt(i);
+                }
+            }
+            foreach (string s in files)
+            {
+                Debug.Log(s);
+            }
+
+
+
+            MD5 md5 = MD5.Create();
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                string file = files[i];
+
+                // hash path
+                string relativePath = file.Substring(path.Length + 1);
+                byte[] pathBytes = Encoding.UTF8.GetBytes(relativePath.ToLower());
+                md5.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                // hash contents
+                byte[] contentBytes = File.ReadAllBytes(file);
+                if (i == files.Count - 1)
+                    md5.TransformFinalBlock(contentBytes, 0, contentBytes.Length);
+                else
+                    md5.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+            }
+            
+            //now hash the result
+            byte[] hash = md5.Hash;
+            string result = BitConverter.ToString(hash).Replace("-", "").ToLower();
+            Debug.Log("MD5: " + result);
+            return result;
+        }        
+
 
         private void AvatarSelectSave()
         {
