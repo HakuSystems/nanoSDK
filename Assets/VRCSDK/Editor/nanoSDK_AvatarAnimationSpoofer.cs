2a1aee4a02fa496897dcbb043044860d872c2a61
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2022-05-28 17:58:21 +0200

    Tarnung.exe dont export this when releese

--- Assets/VRCSDK/Editor/nanoSDK_AvatarAnimationSpoofer.cs
+++ Assets/VRCSDK/Editor/nanoSDK_AvatarAnimationSpoofer.cs
@@ -16,7 +16,7 @@ namespace nanoSDK
 {
     public class nanoSDK_AvatarAnimationSpoofer : EditorWindow
     {
-        [MenuItem("nanoSDK/AvatarAnimationSpoofer", false, 800)]
+        [MenuItem("nanoSDK/Generate Hashes", false, 800)]
         private static void OpenWindow()
         {
             nanoSDK_AvatarAnimationSpoofer window = (nanoSDK_AvatarAnimationSpoofer)EditorWindow.GetWindow(typeof(nanoSDK_AvatarAnimationSpoofer));
@@ -33,41 +33,6 @@ namespace nanoSDK
                 GenerateHashes($"Assets{Path.DirectorySeparatorChar}VRCSDK{Path.DirectorySeparatorChar}nanoSDK"); 
 
             }
-        }
-
-        private void nanoSDKCheckHashes()
-        {
-            string path = $"Assets{Path.DirectorySeparatorChar}VRCSDK{Path.DirectorySeparatorChar}nanoSDK";
-            Uri serverUrl = new Uri("https://www.nanosdk.net/download/Hash/hashes.txt");
-
-            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
-                                 .OrderBy(p => p).ToList();
-
-            var hashFile = File.ReadAllText($"{path}{Path.DirectorySeparatorChar}hashes.txt");
-
-
-            foreach (var file in files)
-            {
-                if (!file.EndsWith(".cs"))
-                {
-                    continue;
-                }
-                var md5 = MD5.Create();
-                var hash = md5.ComputeHash(File.ReadAllBytes(file));
-                var result = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-                //Debug.Log(file + " : " + result);
-                if (!hashFile.Contains(result))
-                {
-                    Debug.LogError("File is not the same: " + file + " : " + result);
-                    if (EditorUtility.DisplayDialog("Error", "Manipulation Detected, Please dont Manipulate nanoSDK Code!", "OK"))
-                    {
-                        //Exit Unity
-                    }
-                }
-
-            }
-
-
         }
 
         public static string GenerateHashes(string path)
